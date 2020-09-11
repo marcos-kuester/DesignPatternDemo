afcc153c04bcc91f43803c80e4879d7c1177604f
Kuester <marcoskuester@gmail.com>
2020-09-11 18:44:08 -0300

    Add project files.

---
+++ 01_Factory/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using static System.Console;
+
+namespace _01_Factory
+{
+
+    public interface Shape
+    {
+        void Draw();
+    }
+
+    public class Square : Shape
+    {
+        public void Draw()
+        {
+            WriteLine("Draw Square");
+        }
+    }
+
+    public class Circle : Shape
+    {
+        public void Draw()
+        {
+            WriteLine("Draw Circle");
+        }
+    }
+
+    public enum ShapeType
+    {
+        Square,
+        Circle
+    }
+
+    public class ShapeFactory
+    {
+        public Shape GetShape(ShapeType shapeType)
+        {
+            switch (shapeType)
+            {
+                case ShapeType.Square: 
+                    return new Square();
+                case ShapeType.Circle:
+                    return new Circle();
+                default:
+                    return null;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShapeFactory shapeFactory = new ShapeFactory();
+
+            var mySquare = shapeFactory.GetShape(ShapeType.Square);
+            mySquare.Draw();
+
+            var myCircle = shapeFactory.GetShape(ShapeType.Circle);
+            myCircle.Draw();
+        }
+    }
+}
